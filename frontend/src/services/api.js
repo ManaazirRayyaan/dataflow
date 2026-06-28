992365b77c6eb7fb9c33c7151f5683e6c6f78247
ManaazirRayyaan <your-new-email@example.com>
2026-06-28 14:40:17 +0530

    backend url correction

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,7 @@
 
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "https://dataflow.manaazirrayyaan.in";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://dataflow-zcdh.onrender.com/";
 
 const api = axios.create({
   baseURL: BASE_URL,
